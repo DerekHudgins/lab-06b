05e72aa1af124dadff459c0ee019305f848a1176
Derek Hudgins <hudgins.derek@gmail.com>
2021-06-16 15:06:11 -0700

    data deployedable to deploy on PGA

--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -1,6 +1,6 @@
 const client = require('../lib/client');
 // import our seed data:
-const animals = require('./animals.js');
+const music = require('./music.js');
 const usersData = require('./users.js');
 const { getEmoji } = require('../lib/emoji.js');
 
@@ -25,12 +25,12 @@ async function run() {
     const user = users[0].rows[0];
 
     await Promise.all(
-      animals.map(animal => {
+      music.map(music => {
         return client.query(`
-                    INSERT INTO animals (name, cool_factor, owner_id)
-                    VALUES ($1, $2, $3);
+                    INSERT INTO music (name, description, category, price, owner_id)
+                    VALUES ($1, $2, $3, $4, $5);
                 `,
-        [animal.name, animal.cool_factor, user.id]);
+        [music.name, music.description, music.category, music.price, user.id]);
       })
     );
     
---
+++ data/music.js
@@ -0,0 +1,69 @@
+module.exports = [
+  {
+    name: 'The Smiths',
+    description: 'A new wave/emo band from the UK',
+    category: 'newwave',
+    price: 15.00
+  },
+  {
+    name: 'The Growlers',
+    description: 'surf rock',
+    category: 'beachgoth',
+    price: 15.00
+  },
+  {
+    name: 'Miles Davis',
+    description: 'Jazz from the 1950s',
+    category: 'Jazz',
+    price: 15.00
+  },
+  {
+    name: 'Streetlight Manifesto',
+    description: 'high energy ska',
+    category: 'ska/punk',
+    price: 15.00
+  },
+  {
+    name: 'The Specials',
+    description: 'first wave ska',
+    category: 'ska',
+    price: 15.00
+  },
+  {
+    name: 'Interpol',
+    description: 'Indi rock from the UK',
+    category: 'Indi Rock',
+    price: 15.00
+  },
+  {
+    name: 'Joy Division',
+    description: 'Sad boi tunes',
+    category: 'First wave emo',
+    price: 15.00
+  },
+  {
+    name: 'The Cure',
+    description: 'Sad boi tunes',
+    category: 'First wave emo',
+    price: 15.00
+  },
+  {
+    name: 'The Cramps',
+    description: 'surf punk',
+    category: 'Pysch/Rock',
+    price: 15.00
+  },
+  {
+    name: 'Snail Mail',
+    description: 'Dreamy',
+    category: 'rock',
+    price: 15.00
+  },
+  {
+    name: 'Angel Olsen',
+    description: 'Dreamy',
+    category: 'Mysitc/dark',
+    price: 15.00
+  },
+
+];
