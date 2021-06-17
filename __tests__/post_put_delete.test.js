3b1c037f2b28683da3a05aec699591800ba8e80c
Derek Hudgins <hudgins.derek@gmail.com>
2021-06-17 14:03:51 -0700

    put test passing

--- __tests__/post_put_delete.test.js
+++ __tests__/post_put_delete.test.js
@@ -64,5 +64,35 @@ describe('post put and delete routes', () => {
 
       expect(dataMusic.body).toContainEqual(newBand);
     });
+    test('/PUT music updates a single music', async() => {
+
+      const data = await fakeRequest(app)
+        .put('/music/6')
+        .send({
+          name: 'Interpol 2',
+          description: 'New version of Interpol',
+          category: 'Indi Rock',
+          price: 16.00
+        })
+        .expect('Content-Type', /json/)
+        .expect(200);
+    
+      const dataMusic = await fakeRequest(app)
+        .get('/music')
+        .expect('Content-Type', /json/)
+        .expect(200);
+    
+      const newBand = { 
+        name: 'Interpol 2',
+        description: 'New version of Interpol',
+        category: 'Indi Rock',
+        price: 16.00,
+        'id': 6, 
+        'owner_id': 1,
+      };
+          
+      expect(data.body).toEqual(newBand);
+      expect(dataMusic.body).toContainEqual(newBand);
+    });
   });
 });
