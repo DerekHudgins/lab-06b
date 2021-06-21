be9f825e3b23aafc018b67d01f4d391d6b3d51af
Derek Hudgins <hudgins.derek@gmail.com>
2021-06-21 14:05:21 -0700

    updated table

---
+++ data/categories.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    name: 'First wave emo'
+  },
+  {
+    name: 'Indi Rock'
+  },
+  {
+    name: 'ska'
+  },
+  {
+    name: 'ska/punk'  
+  },
+  {
+    name: 'Jazz'  
+  },
+  {
+    name: 'beachgoth'  
+  },
+  {
+    name: 'newwave'  
+  },
+];
